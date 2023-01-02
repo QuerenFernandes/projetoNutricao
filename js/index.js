ceed2cf60f1843110def7e234035c2346b927d89
Queren Hapuque <queren.fernandes@yahoo.com>
2023-01-02 19:22:51 -0300

    buscando dados do paciente com js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 /*ALTERANDO O TÍTULO*/
 var titulo = document.querySelector(".container__titulo");
 titulo.textContent = "Tabela de Nutrição";
+
+/*BUSCANDO DADOS DO PACIENTE*/
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
+
+
+console.log(paciente);
+console.log(peso);
+console.log(altura);
+console.log(imc);
