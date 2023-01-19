213dab5e9925d53f136cf9f3e9237fadee26b982
Queren Hapuque <queren.fernandes@yahoo.com>
2023-01-19 07:22:25 -0300

    finalizando requisicao

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -8,14 +8,25 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        console.log(resposta);
-        console.log(typeof resposta); //mostra o tipo do elemento que está sendo buscado, no caso string.
-        var pacientes = JSON.parse(resposta);
 
-       pacientes.forEach(function(paciente){
-        adicionaPacienteNaTabela(paciente);
-       });
+    var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            console.log(resposta);
+            console.log(typeof resposta); //mostra o tipo do elemento que está sendo buscado, no caso string.
+            var pacientes = JSON.parse(resposta);
+    
+           pacientes.forEach(function(paciente){
+            adicionaPacienteNaTabela(paciente);
+           });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            
+            erroAjax.classList.remove("invisivel");
+        }
 
     });
 
